21519bc69b785da4c984468c90d2a50c037b7d70
unknown <nicolasgauvin7@gmail.com>
2019-04-27 15:40:57 +0200

    basic player movement

---
+++ Assets/Scripts/ArcherController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherController : CharacterController
+{
+    private Rigidbody2D rb;
+    private Vector2 moveAmount;
+    private float cooldown;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+    }
+
+    private void Power()
+    {
+        Debug.Log("Archer Power");
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float speed;
+    public float health;
+
+    void Start () {
+        
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
